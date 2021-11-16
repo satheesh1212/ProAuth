7378f4f73a337a6d21cadcd1b7e18acf3a470865
Satheesh Balnem <satheesh.balnem@collabera.com>
2021-11-16 17:50:41 +0530

    latest source

---
+++ src/app/services/Members.ts
@@ -0,0 +1,52 @@
+export class Members
+{
+  mbr_demo_id: any;
+  subscbr_id: any;
+  mbr_ssn: any;
+  mbr_dob: any;
+  mbr_dod: any;
+  mbr_gender: any;
+  mbr_covr_st_dt: any;
+  mbr_covr_end_dt: any;
+  mbr_ethn: any;
+  mbr_race: any;
+  mbr_hm_addr_ln_1: any;
+  mbr_hm_addr_ln_2: any;
+  mbr_hm_county: any;
+  mbr_hm_city: any;
+  mbr_hm_st: any;
+  mbr_hm_zip:any;
+  mbr_tel_ph: any;
+  mbr_email: any;
+  mbr_frst_nm: any;
+  mbr_mi_nm:any;
+  mbr_lst_nm: any;
+
+    constructor(  mbr_demo_id: any,subscbr_id: any,mbr_ssn: any,mbr_dob: any,  mbr_dod: any,  mbr_gender: any,  mbr_covr_st_dt: any, mbr_covr_end_dt: any,
+      mbr_ethn: any,  mbr_race: any,  mbr_hm_addr_ln_1: any,mbr_hm_addr_ln_2: any,  mbr_hm_county: any,  mbr_hm_city: any,  mbr_hm_st: any,  mbr_hm_zip:any,
+      mbr_tel_ph: any,  mbr_email: any, mbr_frst_nm: any,  mbr_mi_nm:any,mbr_lst_nm: any)
+    {
+        this.mbr_demo_id = mbr_demo_id;
+        this.subscbr_id = subscbr_id;
+        this.mbr_ssn = mbr_ssn;
+        this.mbr_dob = mbr_dob;
+        this.mbr_dod = mbr_dod;
+        this.mbr_gender = mbr_gender;
+        this.mbr_covr_st_dt = mbr_covr_st_dt;
+        this.mbr_covr_end_dt = mbr_covr_end_dt;
+        this.mbr_ethn = mbr_ethn;
+        this.mbr_race = mbr_race;
+        this.mbr_hm_addr_ln_1 = mbr_hm_addr_ln_1;
+        this.mbr_hm_addr_ln_2 = mbr_hm_addr_ln_2;
+        this.mbr_hm_county = mbr_hm_county;
+        this.mbr_hm_city = mbr_hm_city;
+        this.mbr_hm_st = mbr_hm_st;
+        this.mbr_hm_zip = mbr_hm_zip;
+        this.mbr_tel_ph = mbr_tel_ph;
+        this.mbr_email = mbr_email;
+        this.mbr_frst_nm = mbr_frst_nm;
+        this.mbr_mi_nm = mbr_mi_nm;
+        this.mbr_lst_nm = mbr_lst_nm;
+    }
+
+}
